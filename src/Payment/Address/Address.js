05646d644d72cebca9860c2458beee39eb6411c9
Ashrita Agarwal <ashritaagarwal.che18@itbhu.ac.in>
2020-12-18 19:10:40 +0530

    Address Page done

--- src/Payment/Address/Address.js
+++ src/Payment/Address/Address.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { UseStateValue } from "../../StateProvider/StateContext";
+import SumbitAddressForm from "./SumbitAddressForm";
 import AddressComponent from "./AddressComponent";
 import "./Address.css";
 const _ = require("lodash");
@@ -8,31 +9,36 @@ function Address() {
   const [address] = UseStateValue().address;
 
   const userAddress = () =>
-    _.toArray(address).map((a) => (
-      <AddressComponent
-        key={a._id || 1}
-        id={a._id}
-        country={a.country}
-        name={a.name}
-        phone={a.phone}
-        house={a.house}
-        area={a.area}
-        pin={a.pin}
-        landmark={a.landmark}
-        city={a.city}
-        state={a.state}
-      />
-    ));
+    address.length === 0 ? (
+      <div className="noAddress">
+        <h3>No Saved Addresses</h3>
+        <div className="addressText">Please add a new Address.</div>
+      </div>
+    ) : (
+      _.toArray(address).map((a) => (
+        <AddressComponent
+          key={a._id || 1}
+          id={a._id}
+          country={a.country}
+          name={a.name}
+          phone={a.phone}
+          house={a.house}
+          area={a.area}
+          pin={a.pin}
+          landmark={a.landmark}
+          city={a.city}
+          state={a.state}
+        />
+      ))
+    );
 
   return (
     <div className="addressMain">
-      {/* <a href="/"> */}
       <img
         src="https://images-eu.ssl-images-amazon.com/images/G/31/x-locale/checkout/checkout-spc-address-banner._CB485947649_.gif"
         alt="amazonLogo"
         className="addressImg"
       />
-      {/* </a> */}
       <h4>Select a delivery address</h4>
       <div className="addressText">
         Is the address you'd like to use displayed below? If so, click the
@@ -40,7 +46,9 @@ function Address() {
         delivery address.
       </div>
       <div className="address">{userAddress()}</div>
-      
+      {address.length>0&&<hr/>}
+
+      <SumbitAddressForm />
     </div>
   );
 }
---
+++ src/Payment/Address/SumbitAddressForm.js
@@ -0,0 +1,140 @@
+import React, { useState } from "react";
+import "./SumbitAddressForm.css";
+import axios from "axios";
+import { UseStateValue } from "../../StateProvider/StateContext";
+
+function SumbitAddressForm() {
+  const [token] = UseStateValue().token;
+  const [address, setAddress] = UseStateValue().address;
+
+  const [formAddress, setFormAddress] = useState({
+    name: "",
+    phone: "",
+    pin: "",
+    house: "",
+    area: "",
+    landmark: "",
+    city: "",
+    state: "",
+    country: "India",
+  });
+
+  const onChangeAddress = (e) => {
+    const { name, value } = e.target;
+    setFormAddress({ ...formAddress, [name]: value });
+  };
+
+  const submitAddress = (e) => {
+    e.preventDefault();
+    console.log(formAddress);
+    axios
+      .post("/address/save", formAddress, {
+        headers: {
+          Authorization: token,
+        },
+      })
+      .then((e) => {
+        console.log(e.data);
+        setAddress([...address,e.data]);
+        window.scrollTo(0,0);
+      })
+      .catch((e) => console.log(e));
+   
+  };
+  return (
+    <div>
+      <form onSubmit={submitAddress} className="addressForm">
+        <h5>Add a new address</h5>
+        <label>Country</label>
+        <div className="addressCountry">India</div>
+        <label htmlFor="name">Full name (First and Last name)</label>
+        <input
+          type="text"
+          name="name"
+          required
+          onChange={onChangeAddress}
+          placeholder=""
+          value={formAddress.name}
+          id="name"
+        />
+        <label htmlFor="phone">Mobile number</label>
+        <input
+          type="text"
+          name="phone"
+          required
+          onChange={onChangeAddress}
+          placeholder="10-digit mobile number without prefixes"
+          value={formAddress.phone}
+          id="phone"
+        />
+        <label htmlFor="pin">PIN code</label>
+        <input
+          type="text"
+          name="pin"
+          required
+          onChange={onChangeAddress}
+          placeholder="6 digit [0-9] pincode"
+          value={formAddress.pin}
+          id="pin"
+        />
+        <label htmlFor="house">
+          Flat, House no., Building, Company, Apartment
+        </label>
+        <input
+          type="text"
+          name="house"
+          required
+          onChange={onChangeAddress}
+          placeholder=""
+          value={formAddress.house}
+          id="house"
+        />
+        <label htmlFor="area">Area, Colony, Street, Sector, Village</label>
+        <input
+          type="text"
+          name="area"
+          required
+          onChange={onChangeAddress}
+          placeholder=""
+          value={formAddress.area}
+          id="area"
+        />
+        <label htmlFor="landmark">Landmark</label>
+        <input
+          type="text"
+          name="landmark"
+          onChange={onChangeAddress}
+          placeholder="E.g. Near AIIMS Flyover, Near SRS Cinema, etc"
+          value={formAddress.landmark}
+          id="landmark"
+        />
+        <label htmlFor="city">Town/City</label>
+        <input
+          type="text"
+          name="city"
+          required
+          onChange={onChangeAddress}
+          placeholder=""
+          value={formAddress.city}
+          id="city"
+        />{" "}
+        <label htmlFor="state">State / Province / Region</label>
+        <input
+          type="text"
+          name="state"
+          required
+          onChange={onChangeAddress}
+          placeholder=""
+          value={formAddress.state}
+          id="state"
+        />{" "}
+        <button className="amazonButton addressSubmit" type="submit">
+          {" "}
+          Submit{" "}
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default SumbitAddressForm;
