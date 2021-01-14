336b05e2ab74fc0c6ca8e060d4581ecb132052f5
Ashrita Agarwal <ashritaagarwal.che18@itbhu.ac.in>
2021-01-15 00:11:33 +0530

    Search Backend + Header

--- src/HeaderFooter/Header.js
+++ src/HeaderFooter/Header.js
@@ -1,22 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.css";
 import SearchIcon from "@material-ui/icons/Search";
 import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
 import { Link } from "react-router-dom";
 import HeaderTogglerMenu from "./HeaderTogglerMenu.js";
 import { UseStateValue } from "../StateProvider/StateContext.js";
+import axios from "axios";
 
 export default function Header() {
   const state = UseStateValue();
   const [name] = state.name;
   const [isLogged] = state.isLogged;
   const [totalItems] = state.totalItems;
+  const [searchText, setSearchText] = useState("");
+
+  const handleSubmitSearch = (event) => {
+    event.preventDefault();
+    console.log(searchText);
+
+    // axios
+    //   .post("/product/search", { searchItem: searchText })
+    //   .then((m) => {
+    //     console.log(m.data);
+    //   })
+    //   .catch((e) => {
+    //     console.log(e);
+    //   });
+  };
 
   return (
     <div className="header">
       <HeaderTogglerMenu />
 
-      {/* fix these Link */}
       <Link to="/" className="headerLink">
         <img
           src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
@@ -25,23 +40,29 @@ export default function Header() {
         />
       </Link>
 
-      <div className="headerSearch">
-        <input type="text" className="headerSearchInput" />
-        <SearchIcon className="headerSearchIcon" />
-      </div>
+      <form className="headerSearch" onSubmit={handleSubmitSearch} name="form">
+        <input
+          type="text"
+          className="headerSearchInput"
+          value={searchText}
+          onChange={(e) => setSearchText(e.target.value)}
+        />
+        <SearchIcon
+          className="headerSearchIcon"
+          role="button"
+          type="submit"
+          onClick={handleSubmitSearch}
+        />
+      </form>
 
       <div className="headerNavbar">
-        <div className="headerNavOption">
-          <img
-            src="https://img.icons8.com/color/25/000000/india.png"
-            alt="languageIcon"
-            className="headerLanguageIcon"
-          />
-        </div>
-
+        <img
+          src="https://img.icons8.com/color/25/000000/india.png"
+          alt="languageIcon"
+          className="headerLanguageIcon"
+        />
         <Link to="/" className="headerLink">
           <div className="headerNavOption">
-            {/*DROPDOWN */}
             <div className="headerNavText1">
               Hello
               {isLogged ? " " + name : ",Sign In"}
@@ -50,28 +71,20 @@ export default function Header() {
           </div>
         </Link>
 
-        <Link to="/" className="headerLink">
+        <Link to="/orders" className="headerLink">
           <div className="headerNavOption">
             <div className="headerNavText1">Returns</div>
             <div className="headerNavText2">& Orders</div>
           </div>
         </Link>
 
-        <Link to="/" className="headerLink">
-          <div className="headerNavOption">
-            {/*DROPDOWN */}
-            <div className="headerNavText1">Try</div>
-            <div className="headerNavText2">Prime</div>
+        <Link to="/checkout" className="headerLink">
+          <div className="headerCart">
+            <ShoppingCartIcon className="headerCartIcon" />
+            <div className="headerCartItems">{totalItems}</div>
           </div>
         </Link>
       </div>
-
-      <Link to="/checkout" className="headerLink">
-        <div className="headerCart">
-          <ShoppingCartIcon className="headerCartIcon" />
-          <div className="headerCartItems">{totalItems}</div>
-        </div>
-      </Link>
     </div>
   );
 }
