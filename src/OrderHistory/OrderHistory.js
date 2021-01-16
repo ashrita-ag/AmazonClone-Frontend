37348cf5081ac31fa52bd6329b6b2a6dc82299dd
Ashrita Agarwal <ashritaagarwal.che18@itbhu.ac.in>
2021-01-16 14:31:32 +0530

    added isLogged to orderHistory page

--- src/OrderHistory/OrderHistory.js
+++ src/OrderHistory/OrderHistory.js
@@ -7,6 +7,7 @@ import Loading from "../Components/Loading";
 
 function OrderHistory() {
   const [token] = UseStateValue().token;
+  const [isLogged] = UseStateValue().isLogged;
   const [orders, setOrders] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
@@ -14,46 +15,54 @@ function OrderHistory() {
   localStorage.removeItem("Payment");
 
   useEffect(() => {
-    setLoading(true);
-    setError("");
-    axios
-      .get("/order/history", { headers: { Authorization: token } })
-      .then((m) => {
-        const errorMsg = m.data.errorMsg;
-        if (errorMsg) setError(errorMsg);
-        else {
-          console.log(m.data);
-          var data = [];
-          data = m.data.map((i) => {
-            return (
-              <OrderComponent
-                key={i._id}
-                cart={i.cart}
-                _id={i._id}
-                total={i.finalcost}
-                gift={i.gift}
-                name={i.address.name}
-                deliveryAddress={i.address}
-                date={i.updatedAt}
-              />
-            );
-          });
-          setOrders(data);
-        }
-      })
-      .catch((e) => {
-        console.log(e);
-        setError(e);
-      });
-    setLoading(false);
+    if (token) {
+      setLoading(true);
+      setError("");
+      axios
+        .get("/order/history", { headers: { Authorization: token } })
+        .then((m) => {
+          const errorMsg = m.data.errorMsg;
+          if (errorMsg) setError(errorMsg);
+          else {
+            console.log(m.data);
+            var data = [];
+            data = m.data.map((i) => {
+              return (
+                <OrderComponent
+                  key={i._id}
+                  cart={i.cart}
+                  _id={i._id}
+                  total={i.finalcost}
+                  gift={i.gift}
+                  name={i.address.name}
+                  deliveryAddress={i.address}
+                  date={i.updatedAt}
+                />
+              );
+            });
+            setOrders(data);
+          }
+        })
+        .catch((e) => {
+          console.log(e);
+          setError(e);
+        });
+      setLoading(false);
+    }
   }, [token]);
 
   return (
     <div className="orderHistory">
       <h4 className="orderHistoryHeading">Your Orders</h4>
-      <div className="errorMsgNew"> {error}</div>
-      {loading ? <Loading /> : orders}
-      {orders.length === 0 && <div>No orders to display</div>}
+      {!isLogged ? (
+        <div>Nothing to display</div>
+      ) : (
+        <div>
+          <div className="errorMsgNew"> {error}</div>
+          {loading ? <Loading /> : orders}
+          {orders.length === 0 && <div>No orders to display</div>}
+        </div>
+      )}
     </div>
   );
 }
